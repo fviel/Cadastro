7fca26ac567f58b49b0154f64a9163baa145e585
fviel <fernandovie82@gmail.com>
2020-06-19 22:58:46 -0300

    complemento da tela de novos clientes

--- resources/views/pages/new_client.blade.php
+++ resources/views/pages/new_client.blade.php
@@ -5,5 +5,18 @@
 @section('body')
 
     <h1>Novo Cliente</h1>
+    <main role="main">
+        <div class="row">
+            <div class="container col-sm-8 offset-md-2">
+                <div class="card border">
+                    <div class="card-header">
+                        <div class="card-title">
+                            Cadastro de Cliente
+                        </div>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </main>
     
 @endsection
